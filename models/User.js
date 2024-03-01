b2e59138b7e405de691fb71f0bc39da891d57c12
yoghurted <poomss2547@gmail.com>
2024-03-02 04:25:18 +0700

    Authentication + User

---
+++ models/User.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name']
+  },
+
+  telephone: {
+    type: String,
+    required: [true, 'Please add a telephone number'],
+    unique: true
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please add a valid email'
+    ]
+  },
+
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    select: false
+  },
+
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
+
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+
+UserSchema.pre('save',async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt);
+});
+
+UserSchema.methods.getSignedJwtToken=function() {
+    return jwt.sign({id:this._id},process.env.JWT_SECRET,{
+        expiresIn: process.env.JWT_EXPIRE
+    }); 
+}
+
+UserSchema.methods.matchPassword=async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password) ;
+}
+
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+
+//route file เอารวมไว้ตรงนี้
+const auth = require('./routes/auth');
+
+//API Security & other
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const hpp = require('hpp');
+const cors = require('cors');
 
 dotenv.config({path:'./config/config.env'});
 
+//connect to db
+connectDB();
+
 const app = express();
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(mongoSanitize());
+
+app.use(helmet());
+
+app.use(xss());
+
+app.use(hpp());
+
+app.use(cors());
+
+//เอา app.use campground ไว้ด้านบนบรรทัดนี้ (ปัง)
+app.use('/api/v1/auth',auth);
+
 
 app.get('/', (req,res) => {
     res.status(200).json({success:true, data:{id:1}});
@@ -11,4 +43,11 @@ app.get('/', (req,res) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+process.on('unhandledRejection',(err,prommise)=>{
+    console.log(`Error: ${err.message}`);
+
+    server.close(()=>process.exit(1));
+
+})
